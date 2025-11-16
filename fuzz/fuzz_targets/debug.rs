b2facfc05c020b7db63cedacd3af52ec9aaa41f2
Slurp9187 <Slurp9187@users.noreply.github.com>
2025-11-15 17:32:29 -0700

    updated fuzz(debug)

--- fuzz/fuzz_targets/debug.rs
+++ fuzz/fuzz_targets/debug.rs
@@ -26,7 +26,8 @@ fuzz_target!(|data: &[u8]| {
         let debug_pw = format!("{:?}", pw);
         assert!(debug_pw.contains("[REDACTED]"));
 
-        if pw_str != "[REDACTED]" {
+        // Skip short strings and any substring of "[REDACTED]"
+        if pw_str.len() > 5 && !"[REDACTED]".contains(pw_str) {
             assert!(!debug_pw.contains(pw_str));
         }
     }
