3c54b98c5bc28889cf65f817a5cff99201faa52b
Slurp9187 <Slurp9187@users.noreply.github.com>
2025-11-20 01:58:04 -0700

    updated macros.rs

--- src/macros.rs
+++ src/macros.rs
@@ -2,23 +2,23 @@
 //
 // Ergonomic constructor macro + From impls
 
-use crate::Secure;
+use crate::secure_gate::SecureGate;
 
 /// Ergonomic constructor macro.
 #[macro_export]
 macro_rules! secure {
     ($ty:ty, $expr:expr) => {
-        $crate::Secure::<$ty>::new($expr)
+        $crate::SecureGate::<$ty>::new($expr)
     };
     ($ty:ty, [$($val:expr),+ $(,)?]) => {
-        $crate::Secure::<$ty>::new([$($val),+])
+        $crate::SecureGate::<$ty>::new([$($val),+])
     };
 }
 
 /// From array sugar.
 macro_rules! impl_from_array {
     ($($N:literal),*) => {$(
-        impl From<[u8; $N]> for Secure<[u8; $N]> {
+        impl From<[u8; $N]> for SecureGate<[u8; $N]> {
             fn from(arr: [u8; $N]) -> Self { Self::new(arr) }
         }
     )*}
