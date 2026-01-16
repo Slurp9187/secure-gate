768e23e06f112816415b86ad203396fbff481891
Slurp9187 <Slurp9187@users.noreply.github.com>
2026-01-16 02:20:36 -0700

    Rename core.rs to secure_encoding_ext.rs and update references

--- src/encoding/extensions/mod.rs
+++ src/encoding/extensions/mod.rs
@@ -15,19 +15,19 @@
 pub mod base64;
 #[cfg(feature = "encoding-bech32")]
 pub mod bech32;
-pub mod core;
 #[cfg(feature = "encoding-hex")]
 pub mod hex;
+pub mod secure_encoding_ext;
 
 #[cfg(feature = "encoding-base64")]
 pub use base64::Base64StringView;
 #[cfg(feature = "encoding-bech32")]
 pub use bech32::Bech32StringView;
+#[cfg(feature = "encoding-hex")]
+pub use hex::HexStringView;
 #[cfg(any(
     feature = "encoding-hex",
     feature = "encoding-base64",
     feature = "encoding-bech32"
 ))]
-pub use core::SecureEncodingExt;
-#[cfg(feature = "encoding-hex")]
-pub use hex::HexStringView;
+pub use secure_encoding_ext::SecureEncodingExt;
