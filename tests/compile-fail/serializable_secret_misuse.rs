35977a8ae5aad2808d2a6cc2dca7e79376130abe
Slurp9187 <Slurp9187@users.noreply.github.com>
2026-03-18 02:25:05 -0700

    Add error annotations to serializable secret misuse test
    Include specific error messages and help text for failed trait bounds
    in the test to document expected compilation failures.

--- tests/compile-fail/serializable_secret_misuse.rs
+++ tests/compile-fail/serializable_secret_misuse.rs
@@ -1,10 +1,19 @@
+// This should fail to compile without the SerializableSecret marker
+
 use secure_gate::{Dynamic, Fixed};
 
-// This should fail to compile without the SerializableSecret marker
 #[derive(serde::Serialize, serde::Deserialize)]
 struct BadSecret(Vec<u8>);
 
 fn main() {
     let _ = serde_json::to_string(&Dynamic::<BadSecret>::new(BadSecret(vec![])));
+    //~^ ERROR the trait bound `BadSecret: zeroize::DefaultIsZeroes` is not satisfied
+    //~| HELP the trait `zeroize::DefaultIsZeroes` is not implemented for `BadSecret`
+    //~| NOTE required for `BadSecret` to implement `zeroize::Zeroize`
+    //~| NOTE required by a bound in `Dynamic`
+
     let _ = serde_json::to_string(&Fixed::<[u8; 32]>::new([0; 32]));
+    //~^ ERROR the trait bound `[u8; 32]: SerializableSecret` is not satisfied
+    //~| HELP the trait `Serialize` is implemented for `Fixed<T>`
+    //~| NOTE required for `Fixed<[u8; 32]>` to implement `Serialize`
 }
